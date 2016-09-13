ff8500478e420b91a7a0b252ce0fc7232bdcd19a
Robert A Petit III <robbie.petit@gmail.com>
2016-09-13 11:40:32 -0400

    added get_samples_by_st function

--- staphopia/R/mlst.R
+++ staphopia/R/mlst.R
@@ -13,3 +13,19 @@ get_mlst_by_sample <- function(sample_ids) {
     request <- '/mlst/srst2/bulk_by_sample/'
     return(submit_post_request(request, sample_ids, chunk_size=50))
 }
+
+#' get_samples_bt_st
+#'
+#' Given a ST, return the sample IDs with MLST (SRST2).
+#'
+#' @param st A integer MLST
+#'
+#' @return Parsed JSON response.
+#' @export
+#'
+#' @examples
+#' get_samples_bt_st(8)
+get_samples_bt_st <- function(st) {
+    request <- paste0('/sample/?st=', format_id(st))
+    return(submit_get_request(request))
+}
