50cc19fac4c29d45af049141fc129e7624cdbd1b
plasmid02 <timothydread@gmail.com>
2016-10-10 14:41:30 -0400

    finished downloads.R for now

--- staphopia/R/downloads.R
+++ staphopia/R/downloads.R
@@ -1,14 +1,24 @@
 #' write_contigs_to_fasta
 #'
-#' @param sample_id
-#' @param fasta_directory
-#' @export write_contigs_to_fasta
+#' @param sample_id (numeric)
+#' @param fasta_directory (string)
+#' @param filt_contigs (numeric) (all contigs with coverage lower than this are removed)
+#' 
+#' @export
 #' @return none
-write_contigs_to_fasta <- function(sample_id,fasta_directory) {
+write_contigs_to_fasta <- function(sample_id,fasta_directory, filt_contigs = 2) {
     library(Biostrings)
     contigs <- get_contigs(sample_id)
-    sa_contigs <- DNAStringSet(contigs$sequence)
-    names(sa_contigs) <- contigs$name
-    path_name <- paste(fasta_directory,a,".fasta",sep="")
+    contigs <- filter(contigs, is_plasmids == FALSE)
+    #adds contig and strain id
+    ctabl <- cbind(contigs$sample,contigs$id,as_data_frame(matrix(unlist(strsplit(contigs$name,"_")), byrow = TRUE, ncol = 6)))
+    keepers <- ctabl$V6 > filt_contigs
+    keepers_names <- sapply(which(keepers), function(x) paste(as.character(ctabl[x,]),collapse = "_"))
+    sa_contigs <- DNAStringSet(contigs$sequence[keepers])
+    names(sa_contigs) <- keepers_names
+    path_name <- paste(fasta_directory,sample_id,".fasta",sep="")
     writeXStringSet(sa_contigs, path_name, append=FALSE, format="fasta")
 }
+
+
+##get plasmids
