2becaa882a2e1f4a2021b78fefe28df2c7c67a0f
Robert A Petit III <robbie.petit@gmail.com>
2016-10-19 16:29:34 -0400

    can filter sample genes by product id, can get info for a given product id

--- staphopia/R/gene.R
+++ staphopia/R/gene.R
@@ -14,3 +14,18 @@ get_gene_products <- function(term=FALSE) {
         return(submit_get_request(paste0('/gene/product/?term=', term)))
     }
 }
+
+#' get_gene_product
+#'
+#' Retrieve gene product infor for a given ID.
+#'
+#' @param product_id An integer product ID
+#'
+#' @return Parsed JSON response.
+#' @export
+#'
+#' @examples
+#' get_gene_product(500)
+get_gene_product <- function(product_id) {
+    return(submit_get_request(paste0('/gene/product/', product_id)))
+}
