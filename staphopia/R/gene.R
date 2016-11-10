31bccb73bd3e7216d178da9356ddb4aa940deb5b
Robert A Petit III <robbie.petit@gmail.com>
2016-11-10 16:46:49 -0500

    fixed empty list resulting as NULL

--- staphopia/R/gene.R
+++ staphopia/R/gene.R
@@ -66,10 +66,15 @@ get_genes <- function(sample_id, product_id=NULL, cluster_id=NULL) {
         }
         json_data <- submit_get_request(request)
         data <- append(data, list(json_data))
-        count <- count + nrow(json_data)
+        if (is.not.null(nrow(json_data))) {
+            count <- count + nrow(json_data)
+
+        }
     }
 
     results <- data.table::rbindlist(data)
+    print(count)
+    print(nrow(results))
     if (count == nrow(results)) {
         return(results)
     } else {
