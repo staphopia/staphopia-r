d70ae20e8462123f4af9ff5fabc782638e568d88
Robert A Petit III <robbie.petit@gmail.com>
2017-06-07 16:30:51 -0400

    get_sccmec_type and get_sccmec_subtype now allow multiple samples to be given

--- staphopia/R/sccmec.R
+++ staphopia/R/sccmec.R
@@ -60,8 +60,6 @@ get_sccmec_subtype_hits <- function(sample_id, exact_hits=FALSE, predict=FALSE) 
 }
 
 
-
-
 #' get_sccmec_type
 #'
 #' Retrieve sccmec type based on primer hits for a given sample.
@@ -73,9 +71,9 @@ get_sccmec_subtype_hits <- function(sample_id, exact_hits=FALSE, predict=FALSE) 
 #'
 #' @examples
 #' get_sccmec_type(500)
+#' get_sccmec_type(c(500,502))
 get_sccmec_type <- function(sample_id) {
-    request <- paste0('/sample/', format_id(sample_id), '/sccmec_primers/?predict')
-    return(submit_get_request(request))
+    return(get_sccmec_primer_hits(sample_id, predict=TRUE))
 }
 
 #' get_sccmec_subtype
@@ -89,48 +87,8 @@ get_sccmec_type <- function(sample_id) {
 #'
 #' @examples
 #' get_sccmec_subtype(500)
+#' get_sccmec_subtype(c(500,502))
 get_sccmec_subtype <- function(sample_id) {
-    request <- paste0('/sample/', format_id(sample_id), '/sccmec_subtypes/?predict')
-    return(submit_get_request(request))
+    return(get_sccmec_subtype_hits(sample_id, predict=TRUE))
 }
 
-#' get_sccmec_subtypes
-#'
-#' Retrieve all predicted sccmec subtypes for a given sample.
-#'
-#' @param sample_id An integer sample ID or A vector of sample IDs
-#' @param product_id Optional integer product ID
-#' @param cluster_id Optional integer cluster ID
-#'
-#' @return Parsed JSON response.
-#' @export
-#'
-#' @examples
-#' get_sccmec_subtypes(500)
-#' get_sccmec_subtypes(c(500, 501, 502)
-#' get_sccmec_subtypes(c(500, 501)))
-get_sccmec_subtypes <- function(sample_id) {
-
-
-    data <- c()
-    count <- 0
-    for (id in sample_id) {
-        request <- paste0('/sample/', format_id(id), '/sccmec_subtypes/?predict')
-        json_data <- submit_get_request(request)
-        data <- append(data, list(json_data))
-        if (is.not.null(nrow(json_data))) {
-            count <- count + nrow(json_data)
-
-        }
-    }
-
-    results <- data.table::rbindlist(data)
-    if (count == nrow(results)) {
-        return(results)
-    } else {
-        print(count)
-        print(nrow(results))
-        return('Error! Count is not equal to number of rows!')
-    }
-
-}
