65eb894b69f8080fc219f0935e24753cce4fb724
Robert A Petit III <robbie.petit@gmail.com>
2017-08-22 19:36:17 -0400

    explicit use of httptest

--- staphopia/tests/testthat/test_connection.R
+++ staphopia/tests/testthat/test_connection.R
@@ -2,8 +2,8 @@ context("Testing API Connection")
 
 
 test_that("We can connect to the API.", {
-    skip_if_disconnected()
+    httptest::skip_if_disconnected()
     url <- build_url('/tests/test_status/')
     json_file <- 'api/tests/test_status.json'
-    expect_json_equivalent(httr::content(httr::GET(url)), jsonlite::fromJSON(json_file))
+    httptest::expect_json_equivalent(httr::content(httr::GET(url)), jsonlite::fromJSON(json_file))
 })
