c7da721026e90489db608098afa61a9141421ba9
Robert A Petit III <robbie.petit@gmail.com>
2018-03-13 18:08:53 -0400

    "get_samples" function is back!

--- staphopia/R/sample.R
+++ staphopia/R/sample.R
@@ -1,3 +1,27 @@
+#' get_samples
+#'
+#' Given a sample ID(s) return sample information.
+#'
+#' @param sample_id A Sample ID
+#'
+#' @return Parsed JSON response.
+#' @export
+#'
+#' @examples
+#' get_samples(5000)
+#' get_samples(c(5000, 5001))
+get_samples <- function(sample_id) {
+    if (is_single_id(sample_id)) {
+        request <- paste0('/sample/', format_id(sample_id), '/')
+        return(submit_get_request(request))
+    } else if (is_multiple_ids(sample_id)) {
+        request <- '/sample/bulk/'
+        return(submit_post_request(request, sample_id, chunk_size=100))
+    } else {
+        warning('sample_id is not the expected type (integer(s) or double(s))')
+    }
+}
+
 #' get_sample_by_name
 #'
 #' Retrieve a sample by its name.
