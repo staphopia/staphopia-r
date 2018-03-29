63e1a6cf45ee71fae93cac4b8711707071de7bc9
Robert A Petit III <robbie.petit@gmail.com>
2018-03-29 13:43:47 -0400

    fixed test cases to reflect new data

--- staphopia/tests/testthat/test_single_sample_endpoints.R
+++ staphopia/tests/testthat/test_single_sample_endpoints.R
@@ -5,7 +5,7 @@ test_that("We can get the info for a single sample.", {
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_sample/')
     json_file <- 'api/tests/test_sample.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
@@ -13,7 +13,7 @@ test_that("We can get the sequencing quality for a single sample.", {
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_quality/')
     json_file <- 'api/tests/test_quality.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
@@ -21,7 +21,7 @@ test_that("We can get the assembly stats for a single sample.", {
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_assembly/')
     json_file <- 'api/tests/test_assembly.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
@@ -29,7 +29,7 @@ test_that("We can get the assembled contigs for a single sample.", {
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_contig/')
     json_file <- 'api/tests/test_contig.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
@@ -37,7 +37,7 @@ test_that("We can get the predicted genes for a single sample.", {
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_gene/')
     json_file <- 'api/tests/test_gene.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
@@ -45,7 +45,7 @@ test_that("We can get the predicted InDels for a single sample.", {
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_indel/')
     json_file <- 'api/tests/test_indel.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
@@ -53,23 +53,15 @@ test_that("We can get the predicted SNPs for a single sample.", {
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_snp/')
     json_file <- 'api/tests/test_snp.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
-test_that("We can get the predicted MLST (SRST2 based) for a single sample.", {
+test_that("We can get the predicted MLST for a single sample.", {
     httptest::skip_if_disconnected()
-    url <- build_url('/tests/test_mlst_srst2/')
-    json_file <- 'api/tests/test_mlst_srst2.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
-    httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
-})
-
-test_that("We can get the predicted MLST (BLAST based) for a single sample.", {
-    httptest::skip_if_disconnected()
-    url <- build_url('/tests/test_mlst_blast/')
-    json_file <- 'api/tests/test_mlst_blast.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    url <- build_url('/tests/test_mlst/')
+    json_file <- 'api/tests/test_mlst.json'
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
@@ -77,7 +69,7 @@ test_that("We can get the SCCmec Primer hits for a single sample.", {
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_sccmec_primer/')
     json_file <- 'api/tests/test_sccmec_primer.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
@@ -85,7 +77,7 @@ test_that("We can get the predicted SCCmec type (Primer based) for a single samp
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_sccmec_primer_predict/')
     json_file <- 'api/tests/test_sccmec_primer_predict.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
@@ -94,7 +86,7 @@ test_that("We can get the SCCmec subtype hits for a single sample.", {
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_sccmec_subtype/')
     json_file <- 'api/tests/test_sccmec_subtype.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
@@ -102,7 +94,7 @@ test_that("We can get the predicted SCCmec subtype (Primer based) for a single s
     httptest::skip_if_disconnected()
     url <- build_url('/tests/test_sccmec_subtype_predict/')
     json_file <- 'api/tests/test_sccmec_subtype_predict.json'
-    response <- httr::content(httr::GET(url), as="text", encoding = "UTF-8")
+    response <- httr::content(httr::GET(url), as="text")
     httptest::expect_json_equivalent(jsonlite::fromJSON(response), jsonlite::fromJSON(json_file))
 })
 
